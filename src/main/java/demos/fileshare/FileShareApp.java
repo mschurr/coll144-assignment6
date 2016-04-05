5646343d0d5959a3b0fb699114ff95e98d176cc4
mschurr <mschurr@rice.edu>
2016-04-05 00:59:22 -0500

    Fixes.

--- src/main/java/demos/fileshare/FileShareApp.java
+++ src/main/java/demos/fileshare/FileShareApp.java
@@ -3,6 +3,9 @@ package demos.fileshare;
 import lightning.Lightning;
 import lightning.util.Flags;
 
+/**
+ * Usage: java FileShareApp --config /path/to/config.json
+ */
 public class FileShareApp {
   public static void main(String[] args) throws Exception {
     Flags.parse(args);
--- src/main/java/demos/tinyurl/TinyUrlApp.java
+++ src/main/java/demos/tinyurl/TinyUrlApp.java
@@ -3,6 +3,9 @@ package demos.tinyurl;
 import lightning.Lightning;
 import lightning.util.Flags;
 
+/**
+ * Usage: java TinyUrlApp --config /path/to/config.json
+ */
 public class TinyUrlApp {
   public static void main(String[] args) throws Exception {
     Flags.parse(args);
